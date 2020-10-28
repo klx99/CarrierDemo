6c68d50446ba42f92a8922fc60dd80e80a9c35b6
klx99 <xiaokun.meng@qcast.cn>
2020-10-28 23:10:24 +0800

    code restruct, add response parser.

--- app/src/main/java/org/elastos/carrier/demo/Logger.java
+++ app/src/main/java/org/elastos/carrier/demo/Logger.java
@@ -1,7 +1,10 @@
 package org.elastos.carrier.demo;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.Spannable;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -17,61 +20,35 @@ public final class Logger {
         sRenderer = renderer;
     }
 
-    public static void info(String msg) {
-        if(Looper.myLooper() != Looper.getMainLooper()) {
-            Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(() -> {
-               info(msg);
-            });
-            return;
-        }
-
-        Log.i(TAG, msg);
-
-        assert(sRenderer != null);
-
-        CharSequence oldMsg = sRenderer.getText();
-        oldMsg = oldMsg + "\n\nI: " + msg;
-        sRenderer.setText(oldMsg);
+    public static void debug(String msg) {
+        log('D', msg);
     }
 
+    public static void info(String msg) {
+        log('I', msg);
+    }
+
+    public static void warn(String msg) {
+        log('W', msg);
+    }
+
+    public static void warn(String msg, Throwable tr) {
+        StringWriter writer = new StringWriter();
+        tr.printStackTrace(new PrintWriter(writer));
+        log('W', msg);
+        log('W', "Excepton: " + writer.toString());
+    }
+
+
     public static void error(String msg) {
-        if(Looper.myLooper() != Looper.getMainLooper()) {
-            Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(() -> {
-                error(msg);
-            });
-            return;
-        }
-
-        Log.e(TAG, msg);
-
-        assert(sRenderer != null);
-
-        CharSequence oldMsg = sRenderer.getText();
-        oldMsg = oldMsg + "\n\nE: " + msg;
-        sRenderer.setText(oldMsg);
+        log('E', msg);
     }
 
     public static void error(String msg, Throwable tr) {
-        if(Looper.myLooper() != Looper.getMainLooper()) {
-            Handler handler = new Handler(Looper.getMainLooper());
-            handler.post(() -> {
-                error(msg, tr);
-            });
-            return;
-        }
-
-        Log.e(TAG, msg, tr);
-
-        assert(sRenderer != null);
-
-        CharSequence oldMsg = sRenderer.getText();
-        oldMsg = oldMsg + "\n\nE: " + msg;
         StringWriter writer = new StringWriter();
         tr.printStackTrace(new PrintWriter(writer));
-        oldMsg = oldMsg + "\nExcepton: " + writer.toString();
-        sRenderer.setText(oldMsg);
+        log('E', msg);
+        log('E', "Excepton: " + writer.toString());
     }
 
     public static void clear() {
@@ -87,6 +64,42 @@ public final class Logger {
 
         sRenderer.setText("");
         sRenderer.scrollTo(0, 0);
+    }
+
+    public static void log(char prefix, String msg) {
+        if(Looper.myLooper() != Looper.getMainLooper()) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(() -> {
+                log(prefix, msg);
+            });
+            return;
+        }
+
+        int color = Color.BLACK;
+        if(prefix == 'I') {
+            Log.d(TAG, msg);
+            color = Color.GREEN;
+        } else if(prefix == 'I') {
+            Log.i(TAG, msg);
+            color = Color.BLUE;
+        } else if(prefix == 'W') {
+            Log.w(TAG, msg);
+            color = Color.YELLOW;
+        } else if(prefix == 'E') {
+            Log.e(TAG, msg);
+            color = Color.RED;
+        }
+
+        assert(sRenderer != null);
+
+        int start = sRenderer.length();
+        sRenderer.append("\n " + prefix + ": ");
+        sRenderer.append(msg);
+        int end = sRenderer.length();
+
+        Spannable spannableText = (Spannable) sRenderer.getText();
+        spannableText.setSpan(new ForegroundColorSpan(color), start, end, 0);
+
     }
 
     private static TextView sRenderer = null;
